fd767a18c9553f08b0b21d778d8b96870a0fb4fe
Eduardo <edureisfaria@gmail.com>
2023-11-21 19:37:43 +0000

    Navigation
    React Navigation Added
    Added Secondary Screen
    Updated App to use Application

--- index.js
+++ index.js
@@ -3,8 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-//import App from './App';
 import {name as appName} from './app.json';
-import App from './src/screens/App';
+import Application from './src';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Application);
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React, { useCallback, useEffect, useState } from "react"
+import NavContainer from "./navigation/container";
+
+const Application = () => {
+    return (
+        <NavContainer />
+    )
+}
+
+export default Application
